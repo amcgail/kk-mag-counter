5d5ef38e2fea80c6ce0f2ac968b46376726f5170
Alec McGail <amcgail2@gmail.com>
2022-02-07 13:04:20 -0500

    am I moving forward?

--- common_imports.py
+++ common_imports.py
@@ -1,3 +1,10 @@
+import sys
+sys.path.insert(0, '/home/jovyan/knowknow')
+from knowknow import *
+
+from os.path import expanduser
+home = expanduser("~")
+
 def genlim(limit):
     def limiter(gen):
         for i,item in enumerate(gen):
@@ -5,3 +12,16 @@ def genlim(limit):
                 break
             yield item
     return limiter
+
+class categorical:
+    def __init__(self):
+        self.items = []
+    def __getitem__(self, x):
+        if type(x) == int:
+            return self.items[x]
+        
+        if x in self.items:
+            return self.items.index(x)
+        
+        self.items.append(x)
+        return len(self.items) - 1
